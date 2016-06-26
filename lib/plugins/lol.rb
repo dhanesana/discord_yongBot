31920915bba8edb4ab06142458663e33d83c65dc
David P. Hanesana <dhanesana@gmail.com>
2016-06-25 23:35:15 -0700

    Add time played to .lol command

--- lib/plugins/lol.rb
+++ lib/plugins/lol.rb
@@ -24,11 +24,12 @@ module YongBot
         deaths = result_2['games'].first['stats']['numDeaths']
         cs = result_2['games'].first['stats']['minionsKilled'] + result_2['games'].first['stats']['neutralMinionsKilled']
         assists = result_2['games'].first['stats']['assists']
+        time = (result_2['games'].first['stats']['timePlayed']/60.00).round(2)
         link_3 = open("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/#{champ_id}?api_key=#{ENV['LOL_KEY']}").read
         result_3 = JSON.parse(link_3)
         champ_name = result_3['name']
         game_result = result_2['games'].first['stats']['win'] ? "Win" : "Loss"
-        event.respond "#{type}(#{game_result}): Level #{level} #{champ_name} => #{kills} Kills, #{deaths} Deaths, #{assists} Assists, #{cs} Creep Kills"
+        event.respond "#{type}(#{game_result}): Level #{level} #{champ_name} => #{kills} Kills, #{deaths} Deaths, #{assists} Assists, #{cs} Creep Kills, #{time} Minutes Played"
       end
 
     end
