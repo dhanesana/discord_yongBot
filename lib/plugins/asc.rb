a8b145ed8e5c5c89ad48f7bc9e755ecea946b5ad
David P. Hanesana <dhanesana@gmail.com>
2018-04-06 23:24:14 -0700

    Use begin-end block with response if there is an exception

--- lib/plugins/asc.rb
+++ lib/plugins/asc.rb
@@ -10,10 +10,14 @@ module YongBot
               description: 'returns summary for upcoming arirang after school club episode',
               usage: '.asc'
               ) do |event|
-        page = Nokogiri::HTML(open('http://www.arirang.co.kr/Tv2/Tv_PlusHomepage_Full.asp?PROG_CODE=TVCR0688&MENU_CODE=101717&sys_lang=Eng'))
-        date = page.css('div.ahtml_h2').text
-        guest = page.css('div.ahtml_h1').text
-        event.respond "After School Club - #{guest} | #{date}"
+        begin
+          page = Nokogiri::HTML(open('http://www.arirang.co.kr/Tv2/Tv_PlusHomepage_Full.asp?PROG_CODE=TVCR0688&MENU_CODE=101717&sys_lang=Eng'))
+          date = page.css('div.ahtml_h2').text
+          guest = page.css('div.ahtml_h1').text
+          event.respond "After School Club - #{guest} | #{date}"
+        rescue Exception => e
+          return event.respond "Error: #{e}"
+        end
       end
 
     end
